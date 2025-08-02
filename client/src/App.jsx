e521d98364ffeb289b0b0d106f4c01d4c8f2696b
Umang <soyumang55@gmail.com>
2025-08-02 12:39:43 +0530

    feat: connected client and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
-
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api");
+      const data = await response.json();
+      setMessage(data.message);
+    };
+    fetchData();
+  }, []);
   return (
-    <>
-      <div className='bg-black text-white'>Hi</div>
-    </>
-  )
+    <div className="flex h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold">{message || "Loading..."}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
@@ -12,8 +12,6 @@ app.use(express.json());
 app.get('/api', (req, res) => {
   res.json({ message: "Hello from the DailyOps backend! 👋" });
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
