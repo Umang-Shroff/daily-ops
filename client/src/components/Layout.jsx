957f7f0292b56248ecf8f65b8937629f4e12d6cc
Umang <soyumang55@gmail.com>
2025-08-03 23:19:06 +0530

    feat: Scroll-to-top btn

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import { CornerLeftUp } from "lucide-react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 const Layout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div className="relative min-h-screen bg-[#f5f2ee] bg-[url('https://www.transparenttextures.com/patterns/climpek.png')] bg-repeat font-sans text-[#1a1a1a]">
@@ -13,6 +21,12 @@ const Layout = () => {
       <main className="pt-20 px-4 md:px-12 lg:px-20 relative z-10">
         <Outlet />
       </main>
+      <button
+        onClick={() => scrollToTop()}
+        className="fixed bottom-6 cursor-pointer right-6 z-50 p-3 bg-[#844d28] text-white rounded-full shadow-lg transition-all hover:bg-[#6d4930] focus:outline-none"
+      >
+        <CornerLeftUp />
+      </button>
     </div>
   );
 };
